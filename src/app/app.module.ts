93ef03ddbd758ff8559221e9c96d8a3248e5792b
Kaily <kaily.lorenz@gmail.com>
2021-10-26 12:21:57 +0300

    angular ngrx

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,62 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core'
+import {BrowserModule} from '@angular/platform-browser'
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import {AppRoutingModule} from './app-routing.module'
+import {AppComponent} from './app.component'
+import {AuthModule} from './auth/auth.module'
+import {StoreDevtoolsModule} from '@ngrx/store-devtools'
+import {StoreModule} from '@ngrx/store'
+import {environment} from '../environments/environment'
+import {HTTP_INTERCEPTORS, HttpClientModule} from '@angular/common/http'
+import {EffectsModule} from '@ngrx/effects'
+import {TopBarModule} from './shared/modules/topBar/topBar.module'
+import {PersistenceService} from './shared/services/persistence.service'
+import {AuthInterceptor} from './shared/services/authinterceptor.service'
+import {GlobalFeedModule} from './globalFeed/globalFeed.module'
+import {routerReducer, StoreRouterConnectingModule} from '@ngrx/router-store'
+import {YourFeedModule} from './yourFeed/yourFeed.module'
+import {TagFeedModule} from './tagFeed/tagFeed.module'
+import {ArticleModule} from './article/article.module'
+import {CreateArticleModule} from './createArticle/createArticle.module'
+import {EditArticleModule} from './editArticle/editArticle.module'
+import {SettingsModule} from "./settings/settings.module";
+import {UserProfileModule} from "./userProfile/userProfile.module";
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AuthModule,
+    HttpClientModule,
+    StoreModule.forRoot({router: routerReducer}),
+    EffectsModule.forRoot([]),
+    StoreDevtoolsModule.instrument({
+      maxAge: 25,
+      logOnly: environment.production,
+    }),
+    StoreRouterConnectingModule.forRoot(),
+    TopBarModule,
+    GlobalFeedModule,
+    YourFeedModule,
+    TagFeedModule,
+    CreateArticleModule,
+    ArticleModule,
+    EditArticleModule,
+    SettingsModule,
+    UserProfileModule,
+    BrowserAnimationsModule
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [
+    PersistenceService,
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: AuthInterceptor,
+      multi: true,
+    },
+  ],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
